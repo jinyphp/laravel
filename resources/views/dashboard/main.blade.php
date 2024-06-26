52575aed2ffeb3cfeb4b8eb4a49a9b56d892a7a0
hojinlee <infohojin@gmail.com>
2024-06-26 12:02:02 +0900

    admin dashboard

---
+++ resources/views/dashboard/main.blade.php
@@ -0,0 +1,24 @@
+<div class="row">
+    <div class="col-3">
+        <div class="card">
+            <div class="card-header">
+                <x-flex-between>
+                    <div>
+                        <h5 class="card-title">
+                            <a href="/admin/laravel/migrations">마이그레이션</a>
+                        </h5>
+                        <h6 class="card-subtitle text-muted">
+                            데이터베이스 마이그레이션을 관리합니다.
+                        </h6>
+                    </div>
+                    <div>
+                        @icon("info-circle.svg")
+                    </div>
+                </x-flex-between>
+            </div>
+            <div class="card-body">
+
+            </div>
+        </div>
+    </div>
+</div>
--- src/Http/Controllers/LaravelDashboard.php
+++ src/Http/Controllers/LaravelDashboard.php
@@ -1,27 +1,36 @@
 <?php
-
 namespace Jiny\Laravel\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
-
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Schema;
 
-class LaravelDashboard extends Controller
+use Jiny\WireTable\Http\Controllers\WireDashController;
+class LaravelDashboard extends WireDashController
 {
     public function __construct()
     {
+        parent::__construct();
+        $this->setVisit($this);
+
+        $this->actions['view']['main'] = "jiny-laravel::dashboard.main";
+
+        $this->actions['title'] = "Laravel Admin";
+        $this->actions['subtitle'] = "JinyPHP Laravel Admin 입니다.";
+
+        //setMenu('menus/site.json');
+        setTheme("admin/sidebar");
     }
 
-
+    /*
     public function index(Request $request)
     {
         return view("jiny-laravel::laravel.dashboard");
     }
+        */
 
 
 }
